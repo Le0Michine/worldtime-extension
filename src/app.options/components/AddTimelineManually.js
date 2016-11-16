ece86317cfbaf42aad21744095c377e03927c0b6
Leo <leomichine@gmail.com>
2016-11-16 08:50:50 +0100

    styled form

--- src/app.options/components/AddTimelineManually.js
+++ src/app.options/components/AddTimelineManually.js
@@ -2,6 +2,8 @@ import React from "react";
 import { connect } from "react-redux";
 import { Field, reduxForm } from 'redux-form';
 
+import style from "./AddTimelineManually.css";
+
 @reduxForm({
   form: "newTimeLineForm"
 })
@@ -10,20 +12,30 @@ export default class AddTimelineManually extends React.Component {
     const { handleSubmit } = this.props;
     return (
       <div>
-        <form onSubmit={handleSubmit}>
-          <div>
-            <label htmlFor="name">Time line name</label>
-            <Field name="name" type="text" component="input" />
+        <form onSubmit={handleSubmit} className={"form-horizontal " + style.topBuffer}>
+          <div className="form-group">
+            <label htmlFor="name" className="control-label col-md-3">Time line name</label>
+            <div className="col-md-6">
+              <Field name="name" type="text" component="input" className="form-control" />
+            </div>
           </div>
-          <div>
-            <label htmlFor="timeZoneName">Time zone name</label>
-            <Field name="timeZoneName" type="text" component="input" />
+          <div className="form-group">
+            <label htmlFor="timeZoneName" className="control-label col-md-3">Time zone name</label>
+            <div className="col-md-6">
+              <Field name="timeZoneName" type="text" component="input" className="form-control" />
+            </div>
           </div>
-          <div>
-            <label htmlFor="timeZoneOffset">Time zone offset</label>
-            <Field name="timeZoneOffset" type="number" component="input" />
+          <div className="form-group">
+            <label htmlFor="timeZoneOffset" className="control-label col-md-3">Time zone offset</label>
+            <div className="col-md-6">
+              <Field name="timeZoneOffset" type="number" component="input" className="form-control" />
+            </div>
           </div>
-          <button>Save</button>
+          <div className="form-group">
+            <div className="col-md-2 col-md-offset-3">
+              <button type="submit" className="btn btn-default">Save</button>
+            </div>
+          </div>
         </form>
       </div>
     );

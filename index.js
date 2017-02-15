888fa5509b71036d423cfdf6fc1916ccd95d3fb7
Le0Michine <leomichine@gmail.com>
2017-02-15 16:10:09 +0100

    update

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function onInstallClick() {
+function onInstallClick(event) {
   // var url = "https://chrome.google.com/webstore/detail/worldtime/ibnegikgmnfnmljianjgnhhlcabonobd";
   // chrome.webstore.install(url, successCallback, failureCallback);
   chrome.webstore.install(undefined, successCallback, failureCallback);
@@ -6,10 +6,16 @@ function onInstallClick() {
 
 function successCallback() {
   console.log("success");
+  document.getElementById("install-button").style.display="none";
+  document.getElementById("link-to-webstore").style.display="block";
 }
 
 function failureCallback(...args) {
   console.log("fail", args);
 }
 
-document.getElementById("install-button").addEventListener("click", onInstallClick);
+if (!chrome.app.isInstalled) {
+  document.getElementById("install-button").addEventListener("click", onInstallClick);
+  document.getElementById("install-button").style.display="block";
+  document.getElementById("link-to-webstore").style.display="none";
+}

5fa11cf4d72aca99f53dcf8cdd9bd4a06a094ac3
Leo <leomichine@gmail.com>
2017-02-15 09:17:08 +0100

    update

---
+++ index.js
@@ -0,0 +1,15 @@
+function onInstallClick() {
+  // var url = "https://chrome.google.com/webstore/detail/worldtime/ibnegikgmnfnmljianjgnhhlcabonobd";
+  // chrome.webstore.install(url, successCallback, failureCallback);
+  chrome.webstore.install(undefined, successCallback, failureCallback);
+}
+
+function successCallback() {
+  console.log("success");
+}
+
+function failureCallback(...args) {
+  console.log("fail", args);
+}
+
+// document.getElementById("install-button").addEventListener("click", onInstallClick);
